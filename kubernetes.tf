2b0343d2215e14f34d63e87b8e66a5b04afacec4
hyferdev <banyerdes@gmail.com>
2023-06-18 14:37:51 +0000

    Initial Kubernetes config

---
+++ kubernetes.tf
@@ -0,0 +1,84 @@
+# Define the IAM roles
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Attach policies to the roles (you can customize this based on your requirements)
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ipv4_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "registry_readonly_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# Create the EKS cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "hyfercluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.27"
+
+  vpc_config {
+    subnet_ids = [aws_subnet.my_subnet_1.id, aws_subnet.my_subnet_2.id]
+  }
+}
+
+# Create the EKS node group
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "hyfernodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids = [aws_subnet.my_subnet_1.id, aws_subnet.my_subnet_2.id]
+  scaling_config {
+    desired_size = 5
+    min_size     = 4
+    max_size     = 7
+  }
+}
+
